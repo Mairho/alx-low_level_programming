bb70a9195a56cd5ce387015d524ad7f4ca2faad5
mairho <marookegbe12@gmail.com>
2022-09-08 15:32:59 -0700

    task 5 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <studio.h>
+
 /**
  * main - print the string in the put option
  *
  * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle"
+ * this program prints "programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
