d66a131f1833a36d3f8f5bc7ffc6d0a624d5bf92
mairho <marookegbe12@gmail.com>
2022-09-11 09:11:32 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - printing the starting funtion
-*
-* Description: using main function
-* this program print "programming is like buliding a multilingual puzzle
-* Return: 0
-*/
+ *main - printing the starting funtion
+ *
+ * Description: using main function
+ * this program print "programming is like buliding a multilingual puzzle
+ * Return: 0
+ */
 int main(void)
 {
      puts("\"Programming is like building a multilingual puzzle");
