f12174fb90d4966bb71ec7baee0738b5eb7f1410
mairho <marookegbe12@gmail.com>
2022-09-11 09:50:41 -0700

    task 5 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,11 @@
 /**
 *main - print the string in the put function
 *
-* Description - Function to send output to stdout vua printf()
-* Return: Returns function
+*Description - Function to send output to stdout vua printf()
+*Return: Returns function
 */
 int main(void)
 {
-     printf("with proper grammar, but the outcome is a piece of art,\n*);
-        return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n*);
+return (0);
 }
-
