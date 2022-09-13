daeafc90c8529f20b9c8c628b6e2d82f91c5b16e
mairho <marookegbe12@gmail.com>
2022-09-13 13:47:05 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,81 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/**
+* _putchar - writes the character c to std
+* @c: parameter to compare
+* Return: 1 or 0
+*/
+int _putchar(char c);
+/**
+* print_alphabet - function to print alphabet
+*
+*/
+void print_alphabet(void);
+/**
+* print_alphabet_x10 - print alphabet 10 times
+*
+*/
+void print_alphabet_x10(void);
+/**
+* _islower - prints if c is lower case
+* @c: character to use
+* Return: 1 or 0
+*/
+int islower(int c);
+/**
+* _isalpha - prints if character is alphabet
+* @c: character to use
+* Return: 1 or 0
+*/
+int isalpha(int c);
+/**
+* print_sign - prints a sign based on n
+* @n: integer to input
+* Return: 1 or 0 or -1
+*/
+int print_sign(int n);
+/**
+* _abs - gives absolute value
+* @int: integer
+* Return: interger or abs value of int
+*/
+int _abs(int);
+/**
+* print_last_last - prints digit last
+*
+* @int: integer to input
+* Return: last digit of integer
+*/
+int print_last_digit(int);
+/**
+* jack_bauer - jack bauer gooo
+*
+* Return: return jack bauer
+*/
+void jack_bauer(void);
+/**
+*times_table - times this up
+*
+* Return: return a multiple
+*/
+void times_table(void);
+/**
+* add - adds stuff up
+*
+* @int: integers to add
+* Return: returns a sum
+*/
+int add(int, int);
+/**
+* print_to_98 - prints to 98
+* @n: integer to start at
+*
+*/
+void print_to_98(int n);
+/**
+* print_times_tabe - whatever man
+* @n: integer to put in this table
+*
+*/
+void print_times_tables(int n);
+#endif
