1fe3031fbeb75a1ef5011c2e1cf6c67354ac69a2
mairho <marookegbe12@gmail.com>
2022-09-15 16:02:33 -0700

    hhh

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - program that prints either number
+*or fizz or buzz or fizzBuzz
+*
+*
+*Return: returns 0
+*/
+
+int main(void)
+{
+	int num = 1;
+	
+	while (num++ < 100)
+	{
+		if ((num % 3 ==
