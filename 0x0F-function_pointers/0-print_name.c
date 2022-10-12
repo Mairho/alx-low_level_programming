3692fccd1e7e7e6a0b25f6e96a896bea95532328
mairho <marookegbe12@gmail.com>
2022-10-12 15:24:36 -0700

    git

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
+
 /**
 * print_name - prints a name
 * @name: pointer to name
@@ -11,6 +12,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	
 	if (name != NULL && f != NULL)
-
 		f(name);
 }
