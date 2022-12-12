87708ab3ba3cb179e0474c35a3d5ebe80c0a45a4
mairho <marookegbe12@gmail.com>
2022-12-12 11:17:06 -0800

    j

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Mairho/alx-low_level_programming/raw/master/0x18-dynamic_libraries/iwin.so
+wget -P /tmp https://github.com/Mairho/alx-low_level_programming
+/raw/master/0x18-dynamic_libraries/iwin.so
 export LD_PRELOAD=/tmp/iwin.so
